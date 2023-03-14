4e3bbb1afa53989f5c8a050a10025196131dae5f
Drevlyanin <121987689+Drevlyanin@users.noreply.github.com>
2023-03-14 21:42:12 +0200

    Add files via upload

--- weather/models.py
+++ weather/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class City(models.Model):
     name = models.CharField(max_length=30)
 
-    def __srt__ (self):
+    def __str__ (self):
         return self.name
--- weather/views.py
+++ weather/views.py
@@ -1,11 +1,18 @@
 import requests
 from django.shortcuts import render
 from .models import City
+from .forms import CityForm
 
 
 def index(request):
     appid = '72b9615007389dcb29cff9e568aea91a'
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+
+    if(request.method == 'POST'):
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
 
     cities = City.objects.all()
 
@@ -21,6 +28,6 @@ def index(request):
 
         all_cities.append(city_info)
 
-    context = {'all_info': city_info }
+    context = {'all_info': all_cities, 'form': form}
 
     return render(request, 'weather/index.html', context)
