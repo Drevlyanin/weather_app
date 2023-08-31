ee92e6f3172be6e7b7671fc73e4b5ebaeb26d0b4
Drevlyanin <121987689+Drevlyanin@users.noreply.github.com>
2023-08-31 23:59:12 +0300

    Update settings.py

--- weathersite/settings.py
+++ weathersite/settings.py
@@ -4,8 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = ''
